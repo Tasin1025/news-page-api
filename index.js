43f84d62eb68767a44b5ca143bfe0db7bbe08fbf
Tasin1025 <tasintt@gmail.com>
2023-09-02 22:08:33 +0600

    api fetched

---
+++ index.js
@@ -0,0 +1,9 @@
+const handleCategory = async () => {
+
+    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await response.json();
+    console.log(data);
+    
+}
+
+handleCategory();
